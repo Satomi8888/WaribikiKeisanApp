04bbd9b26ffb91cc3bce9f522f004f2f07724cd4
遠山聡美 <S083@inv-238.inv.local>
2017-10-18 19:30:06 +0900

    1018ボタン作り始めたよ

--- myCalc2/ViewController.swift
+++ myCalc2/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var priceField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapActionButton(_ sender: Any) {
+        let value = priceField.text! + "1"
+        //print(Int(value))
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
 
+    @IBAction func tap0Button(_ sender: Any) {
+        let value = priceField.text! + "0"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap00Button(_ sender: Any) {
+        
+    }
+    
+    @IBAction func tapClearButton(_ sender: Any) {
+            priceField.text = "0"
+    }
 }
 
